9ba39ad24c594e11abaa613f8cc39b68be48c538
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-04 00:53:17 +0530

    Add navigationRef for passing navigation details

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import {
-  createAppContainer,
-  createStackNavigator,
-  createBottomTabNavigator,
-  createSwitchNavigator
+	createAppContainer,
+	createStackNavigator,
+	createBottomTabNavigator,
+	createSwitchNavigator
 } from 'react-navigation';
 
 import TestScreen from './src/screens/TestScreen';
 
+import { setNavigator } from './src/navigationRef';
+
+
 const switchNavigator = createSwitchNavigator({
-  Test: TestScreen
+	Test: TestScreen
 });
 
 const App = createAppContainer(switchNavigator);
 
 export default () => {
-  return (
-    <App/>
-  )
+	return (
+		<App
+			ref={(navigator) => {
+				setNavigator(navigator);
+			}}
+		/>
+	)
 }
