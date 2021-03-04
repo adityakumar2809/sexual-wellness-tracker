1f3ea404f6518780137e9fb1d3e7948124e811d7
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-03-05 01:22:02 +0530

    Add basic layout of signin page

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+    Text,
     View,
-    Text
-} from 'react-native';
+    TextInput,
+    Button,
+    StyleSheet
+} from 'react-native'
 
 
-const SignInScreen = () => {
+const SignInScreen = ({ navigation }) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View>
             <Text>
                 Hello World! This is SignInScreen
             </Text>
+
+            <Text>Email</Text>
+            <TextInput
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.textInputStyle}
+            />
+
+            <Text>Password</Text>
+            <TextInput
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+                style={styles.textInputStyle}
+            />
+
+            <Button
+                title='Submit'
+                onPress={() => console.log({ email, password })}
+            />
+
+            <Button
+                title='SignUp Instead'
+                onPress={() => navigation.navigate('SignUp')}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    textInputStyle: {
+        borderColor: '#000000',
+        borderWidth: 2,
+        margin: 10
+    }
+});
 
 export default SignInScreen;
